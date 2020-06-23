8eaa7cbc4c42c038e4f3d1865600b5b2a858bdd6
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-06-23 15:06:23 -0500

    Modified url to get fahrenheit and celsius

--- src/city.js
+++ src/city.js
@@ -1,11 +1,18 @@
 const City = (() => {
+  const weatherKey = '4cc6db5c47a2fa3608fe302dad1dd2dc';
   async function getCity(city) {
-    const cityWeather = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4cc6db5c47a2fa3608fe302dad1dd2dc`,
+    const fahrenheit = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}&units=imperial`,
       { mode: 'cors' }
     );
-    const temperature = cityWeather.json();
-    console.log(temperature);
+    const celsius = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}&units=metric`,
+      { mode: 'cors' }
+    );
+    const temperatureF = fahrenheit.json();
+    const temperatureC = celsius.json();
+
+    console.log(temperatureF, temperatureC);
   }
 
   return { getCity };
